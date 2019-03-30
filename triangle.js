ca2cfae1a094c691a7b3f42ec503f40f4316f475
naisenyamungai <naisenyamungai@gmail.com>
2019-03-30 11:58:38 +0300

    display content in .html file

--- triangle.js
+++ triangle.js
@@ -10,22 +10,22 @@ function scanInput(){var triangleSides = [];
     triangleSides.sort((a,b) => a - b);
     
     /**The algorithm below will check the type of triangle based on the conditions using if, else if and else statements */
-    if(triangleSides[0] + triangleSides[1] < triangleSides[2]){
-       text = triangleSides + " " + "is NOT a triangle";
+    if((triangleSides[0] + triangleSides[1]) <= triangleSides[2]){
+       text = "Ooops!" + " " + triangleSides + " " + "is NOT a triangle! <br><br><br><br>For a triangle to be formed, the sum of the two smaller side must be  greater than the largest side";     
     }
     else if(triangleSides[0] % triangleSides[1] === 0 && triangleSides[0] % triangleSides[2] === 0 ){
-        text = triangleSides + " " + "is an EQUILATERAL Triangle";
+        text = triangleSides + " " + "is an EQUILATERAL Triangle! <br><br><br><br>For an Equilateral Triangle all the three sides are of Equal Length and the three angles are all 60 degrees.";
     
     }
     else if((triangleSides[0] - triangleSides[1] === 0 && triangleSides[0] - triangleSides[2] !== 0) && (triangleSides[0] + triangleSides[1] > triangleSides[2])){
-        text = triangleSides + " " + "is an ISOSCELES Triangle";
+        text = triangleSides + " " + "is an ISOSCELES Triangle ! <br><br><br><br> Isosceles triangle has two equal sides, and the sum of the two sides must always be greater than the largest side." ;
     
     }
     else if(triangleSides[0] - triangleSides[1] - triangleSides[2] !== 0 && triangleSides[0] + triangleSides[1] > triangleSides[2]){
-        text = triangleSides + " " + "is a SCALENE Triangle";
+        text = triangleSides + " " + "is a SCALENE Triangle ! <br><br><br><br> For a scalene, all the sides are different and so are the angles.";
 
     }
-    else{text = "Check on special Triangles like Right Angled Triangle & Obtuse Triangle";
+    else{text = "Ooops!!! Check whether the sum of the angles is equal to 180!";
 }
 /** display the content on html page */
     document.getElementById("display").innerHTML=text;
